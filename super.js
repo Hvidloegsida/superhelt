a8454823908b482bf75db08572b26bfdbb0f3a37
Hvidloegsida <idmi0001@stud.kea.dk>
2023-08-27 16:57:40 +0200

    flyve

--- super.js
+++ super.js
@@ -13,11 +13,23 @@ function showsuperhelt(superhelt) {
 
   copy.querySelector("h1").textContent = superhelt.alias;
   copy.querySelector("h3").textContent = superhelt.realName;
+  if (superhelt.powers.includes("flyve") || superhelt.powers.includes("flying") || superhelt.powers.includes("Fly")) {
+    copy.querySelector("article").classList.add("flyve");
+  }
   copy.querySelector("#power").textContent = superhelt.powers;
   copy.querySelector("#origin").textContent = superhelt.origin;
   copy.querySelector("#weakness").textContent = superhelt.weaknesses;
   copy.querySelector("#active").textContent = superhelt.active;
-  copy.querySelector("#evil").textContent = superhelt.isEvil;
+
+  /*I MIN CSS KAN MAN SE AT MIN CLASS EVIL IKKE ER SYNLIG. 
+  DEREFTER ER DER EN KLASSE HVOR DEN SIGER AT HVIS MAN HAR CLASSEN EVILTRUE, SÅ ER DEN SYNLIG.
+  HER SIGER MIN JS SÅ, HVIS EVIL ER TRUE ----*/
+  if (superhelt.isEvil) {
+    /*SÅ SKAL eviltrue TILFØJES TIL MIT DOM ELEMENT AKA GØR DEN SYNLIG*/
+    copy.querySelector("article").classList.add("eviltrue");
+    /*HER FORTÆLLER JEG DEN SÅ AT DEN IKKE SKAL SKRIVE TRUE, MEN BARE SKRIVE EVIL*/
+    copy.querySelector("#evil").textContent = "Evil"; // Set the text content to "evil"
+  }
   copy.querySelector("#height").textContent = superhelt.height;
   copy.querySelector("#birthday").textContent = superhelt.birthday.day + "/" + superhelt.birthday.month + "/" + superhelt.birthday.year;
   copy.querySelector("img").src = superhelt.image;
